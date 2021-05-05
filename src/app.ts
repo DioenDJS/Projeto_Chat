84fd4476e008c6ba9c0bc0d3a41542849692907c
DioenDJS <negociospacheti@gmail.com>
2021-05-05 12:29:36 -0300

    crinado o diretorio public que vai ficar com a parte do client com os arquivos html css js

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import { createServer } from "http";
+import { Server, Socket} from "socket.io";
 import { routes } from "./routes"
 
 const app = express();
+
+const http = createServer(app);// Criando protocolo http
+const io = new Server(http);//Criando protocolo WS
+
+io.on("connection", (Socket: Socket) =>{
+    console.log("Se conectou", Socket.id);
+});
 
 //antes das rotas
 app.use(express.json());
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import "reflect-metadata";
 import "./database";
+import { createServer } from "http";
+import { Server, Socket} from "socket.io";
 import app from './app';
 
-app.listen(3333, () =>{
+const http = createServer(app);// Criando protocolo http
+const io = new Server(http);//Criando protocolo WS
+
+io.on("connection", (Socket: Socket) =>{
+    console.log("Se conectou", Socket.id);
+});
+
+http.listen(3333, () =>{
     
     console.log("servidor esta rodando !!! ");
-});
+});// pasusando no minuto 18:35
