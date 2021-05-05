15d29cfd5e6cdb2b7600121a6f82ebcbeea3a8e7
DioenDJS <negociospacheti@gmail.com>
2021-05-05 15:51:36 -0300

    criando a view do projeto

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
-
+import path from "path";
 import { routes } from "./routes"
 
 const app = express();
 
+//conectando a pasta public para fazer as vezes client na aplicação
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
+//utilizando uma rota com o metodo get para renderizar a pagina
+app.get("/pages/client", (request, response) =>{
+    return response.render("html/client.html");
+})
 
 //antes das rotas
 app.use(express.json());
